552f238e7108fb01c82eb334906ed70724181487
Brad Ploeger <bploeger@arkturis.com>
2014-09-28 10:57:29 -0400

    Updated Angular Listings

--- park/js/listing.js
+++ park/js/listing.js
@@ -1,61 +1,53 @@
-var taReviews = null;
-//AngularJS Controllers
-function reviewCtrl($scope, $resource) {
+//Angular App
+var app = angular.module('RVP', ['ngResource']);
+
+app.factory("TAReviews", function($resource) {
+  return $resource("http://api.tripadvisor.com/api/partner/2.0/location/:id?key=42cc155371794d1eb2d922da2de694c3");
+});
+
+app.controller('ReviewCtrl', function($scope, $resource, TAReviews) {
   
-  $scope.reviewFld = "";
-  $scope.reviewAuthor = "";
-  $scope.reviewStars = "";
+    TAReviews.get({id: 1145019}, function(data) {
+    console.log(data.reviews);
+    $scope.reviews = data.reviews;
+  });
+});
 
-  var taAPI = $resource("http://api.tripadvisor.com/api/partner/2.0/location/1145019?key=42cc155371794d1eb2d922da2de694c3",
-                { callback: "JSON_CALLBACK" },
-                { get: {method: "JSONP"}};
+//Facebook App
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '445464698925742',
+      xfbml      : true,
+      version    : 'v2.1'
+    });
+  };
 
-  $scope.reviews = taAPI.get();
-
-
- /*$scope.reviews = [
-    {
-      text: "We stayed in one of the cabins and it was very clean and had some linens as well as kitchenware which was surprising. The overall campground was very clean and the pool area was wonderful for the kids. It was nice to have a small pool setup just for the little kids along with a larger pool and hot tub. They also had a large chess/checkers board, bounce pad, and other activities. The staff was very helpful in recommending a horseback riding trip and wanted to know if it went well for us afterwards.",
-      author: "Trip Advisor User",
-      stars: "5", 
-      done:"false"
-    },{
-      text: "Stayed for two nights of camping with my 8 year old daughter. The campsites were adequate and clean. The pancake breakfast was a hit for sure...the pancakes are free, but there is a nominal charge for beverages and sausage (by no means outrageous). My daughter had a blast jumping on the bouncing pillow, swimming in the pool, and playing with the over sized chess/checker set. They also have an over sized Scrabble board set with benches and shade, and fun little four wheeled bicycles for rental ($5 for 30 minutes). I would say that the only issue we had was with the number of toilet/shower facilities available. Each morning there was quite a lengthy line to shower and use the restrooms. The staff was very friendly and inviting.", 
-      author: "mpbesaw",
-      stars: "4",
-      done:"false"
-    }
-  ];*/
-
-  $scope.addReview = function () {
-    
-    if ($scope.reviewFld != "") {
-      $scope.reviews.push({text: $scope.reviewFld, author: $scope.reviewAuthor, stars: $scope.reviewStars, val: $scope.valFld });
-      $scope.reviewFld = "";
-    }
-  };  
-}
-
-
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
 
 //Bootstrap Init Code
     $('#main a').click(function (e) {
         e.preventDefault()
       $(this).tab('show')
-    })
+    });
     $('#media a').click(function (e) {
         e.preventDefault()
       $(this).tab('show')
-    })
+    });
     $('#map a').click(function (e) {
         e.preventDefault()
       $(this).tab('show')
-    })
+    });
     $('#social a').click(function (e) {
         e.preventDefault()
       $(this).tab('show')
-    })
+    });
     $('#reviews a').click(function (e) {
         e.preventDefault()
       $(this).tab('show')
-    })
+    });
